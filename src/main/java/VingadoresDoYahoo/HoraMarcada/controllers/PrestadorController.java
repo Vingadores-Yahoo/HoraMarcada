ac24913c291fbf2a5109977278ff14546db3770b
Marcelo <marcelobleal@yahoo.com.br>
2021-09-20 21:43:14 -0300

    Modificação pagina de perfil publico do prestador

--- src/main/java/VingadoresDoYahoo/HoraMarcada/controllers/PrestadorController.java
+++ src/main/java/VingadoresDoYahoo/HoraMarcada/controllers/PrestadorController.java
@@ -1,11 +1,14 @@
  package VingadoresDoYahoo.HoraMarcada.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -71,6 +74,15 @@ public class PrestadorController {
     	return mv;
     }
 
+    @GetMapping("/prestadores/{id}")
+    public ModelAndView prestrador(@PathVariable Long id){
+        Optional<Prestador> prestadorOptional = prestadorRepository.findById(id);
+    	ModelAndView mv = new ModelAndView();
+        mv.addObject("prestador", prestadorOptional.get());
+    	mv.setViewName("infoPrestador");
+    	return mv;
+    }
+
     @PostMapping(path = "/cadastroPrestador")
     public String salvarPrestador(@Validated CadastroPrestador cadastroPrestador, BindingResult br) throws Exception {
         if(br.hasErrors()){
@@ -91,4 +103,4 @@ public class PrestadorController {
     
     }
 
-} 
+}
